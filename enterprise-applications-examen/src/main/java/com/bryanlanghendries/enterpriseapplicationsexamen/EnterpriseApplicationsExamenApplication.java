cce36955e026f427da6167a88ef0c28f2729ce93
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2024-04-12 14:47:55 +0200

    init

---
+++ enterprise-applications-examen/src/main/java/com/bryanlanghendries/enterpriseapplicationsexamen/EnterpriseApplicationsExamenApplication.java
@@ -0,0 +1,13 @@
+package com.bryanlanghendries.enterpriseapplicationsexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseApplicationsExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseApplicationsExamenApplication.class, args);
+	}
+
+}
---
+++ enterprise-applications-examen/src/test/java/com/bryanlanghendries/enterpriseapplicationsexamen/EnterpriseApplicationsExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryanlanghendries.enterpriseapplicationsexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationsExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
