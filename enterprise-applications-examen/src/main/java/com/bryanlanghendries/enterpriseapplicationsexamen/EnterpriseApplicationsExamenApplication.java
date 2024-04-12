d08995b7ed4664026317b1f875f6e78ba256eb93
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2024-04-12 15:58:37 +0200

    Fixed dependencies and connected database

--- enterprise-applications-examen/src/main/java/com/bryanlanghendries/enterpriseapplicationsexamen/EnterpriseApplicationsExamenApplication.java
+++ enterprise-applications-examen/src/main/java/com/bryanlanghendries/enterpriseapplicationsexamen/EnterpriseApplicationsExamenApplication.java
@@ -2,8 +2,16 @@ package com.bryanlanghendries.enterpriseapplicationsexamen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bryanlanghendries")
+@EntityScan("com.bryanlanghendries.repository.entities")
+@PropertySource(value = "classpath:application.properties")
+@ComponentScan("com.bryanlanghendries")
+@EnableJpaRepositories("com.bryanlanghendries.repository")
 public class EnterpriseApplicationsExamenApplication {
 
 	public static void main(String[] args) {
