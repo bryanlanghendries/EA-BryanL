76a27c1b732e5729bc9018e374acae70d8e61c96
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2024-04-13 16:01:25 +0200

    Update DtoResponseExceptionHandler.java

--- enterprise-applications-examen/src/main/java/com/bryanlanghendries/exceptions/handler/DtoResponseExceptionHandler.java
+++ enterprise-applications-examen/src/main/java/com/bryanlanghendries/exceptions/handler/DtoResponseExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.bryanlanghendries.exceptions.handler;
 
+import com.bryanlanghendries.exceptions.BadInputException;
 import com.bryanlanghendries.exceptions.EntityNotFoundException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Log4j2
 public class DtoResponseExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity handlEntityNotFound(final EntityNotFoundException ex){
-        log.error("Error occured: ", ex);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Void> handleEntityNotFound(final EntityNotFoundException ex){
+        log.error("Error occurred: ", ex);
+        return ResponseEntity.status(204).build();
+    }
+
+    @ExceptionHandler(BadInputException.class)
+    public ResponseEntity<Void> handleBadInput(final BadInputException ex){
+        log.error("Error occurred: ", ex);
+        return ResponseEntity.status(400).build();
     }
 }
